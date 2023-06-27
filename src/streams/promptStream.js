a6c6c5cff789bba1f8512c3835c8fb3cb73292bf
Camilo Jaramillo <80411997+camiloJaramillo5@users.noreply.github.com>
2023-06-26 22:29:53 -0500

    Delete promptStream.js

--- src/streams/promptStream.js
+++
@@ -1,7 +0,0 @@
-import { BehaviorSubject } from 'rxjs';
-
-// Create a BehaviorSubject to hold the data
-const PromptStream = new BehaviorSubject('');
-
-// Export the stream
-export default PromptStream;
