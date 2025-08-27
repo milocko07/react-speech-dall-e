781472c0e854c75f27f98f7661dac8f4cfcacb77
Camilo Jaramillo <camiloinfo07@gmail.com>
2025-08-26 23:32:49 -0500

    Update Heading.js

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,7 @@
 export function Heading() {
     return (
         <div className="p-2">
-            <h2>Generate Inclusiveness DALL-E images using the OPENAI API</h2>
+            <h2>Generate Inclusiveness DALL-E images using the OpenAi API</h2>
         </div>
     )
 }
